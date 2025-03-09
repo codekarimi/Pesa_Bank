2c84812a1d3453857e55f2fb1cdfd9246cabe79f
codekarimi <ckarimi676@gmail.com>
2025-03-09 16:36:32 +0300

    Donut chart.js

---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const accountNames = accounts.map((a) => a.name);
+  const balances = accounts.map((a) => a.currentBalance)
+
+  const data = {
+    datasets: [
+      {
+        label: 'Banks',
+        data: [1,2,3,4],
+        // data: balances,
+        backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'] 
+      }
+    ],
+    // labels: accountNames
+    labels: ["heloo","good"]
+  }
+
+  return <Doughnut 
+    data={data} 
+    options={{
+      cutout: '60%',
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }}
+  />
+}
+
+export default DoughnutChart
